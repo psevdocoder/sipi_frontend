8e0636c983e97f8e6b255a7a8b27762a220e5e95
Maksim Kautarov <just5py@yandex.ru>
2023-04-25 17:27:41 +0300

    navbar after login fix

--- sipi_front/src/App.vue
+++ sipi_front/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <AppHeader v-if="isAuthenticated" />
-    <router-view />
-    <AppFooter />
-  </div>
+    <div>
+        <AppHeader :isAuthenticated="isAuthenticated" />
+        <router-view />
+        <AppFooter />
+    </div>
 </template>
 
 <script>
@@ -13,41 +13,44 @@ import { computed, onMounted } from "vue";
 import { useRouter } from "vue-router";
 
 export default {
-  name: "App",
-  components: {
-    AppFooter,
-    AppHeader,
-  },
-  setup() {
-    const router = useRouter();
+    name: "App",
+    components: {
+        AppFooter,
+        AppHeader,
+    },
+    setup() {
+        const router = useRouter();
 
-    // проверяем, авторизован ли пользователь
-    const isAuthenticated = computed(() => {
-      const token = getCookieValue("jwt");
-      return token !== null && token !== "";
-    });
+        // проверяем, авторизован ли пользователь
+        const isAuthenticated = computed(() => {
+            const token = getCookieValue("jwt");
+            return token !== null && token !== "";
+        });
 
-    // проверяем, нужно ли перенаправить пользователя на страницу входа
-    onMounted(() => {
-      if (!isAuthenticated.value) {
-        router.push("/login");
-      }
-    });
+        // проверяем, нужно ли перенаправить пользователя на страницу входа
+        onMounted(() => {
+            if (!isAuthenticated.value) {
+                router.push("/login");
+            }
+        });
 
-    const getCookieValue = (name) => {
-      const cookies = document.cookie.split(";");
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        if (cookie.startsWith(name + "=")) {
-          return cookie.substring(name.length + 1);
-        }
-      }
-      return null;
-    };
+        const getCookieValue = (name) => {
+            const cookies = document.cookie.split(";");
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                if (cookie.startsWith(name + "=")) {
+                    return cookie.substring(name.length + 1);
+                }
+            }
+            return null;
+        };
 
-    return {
-      isAuthenticated,
-    };
-  },
+        return {
+            isAuthenticated,
+        };
+    },
 };
 </script>
+
+<style scoped>
+</style>
--- sipi_front/src/components/AppHeader.vue
+++ sipi_front/src/components/AppHeader.vue
@@ -1,31 +1,36 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <div class="container-fluid">
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link"> Home </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/subjects" class="nav-link"> Subjects </router-link>
-          </li>
-        </ul>
-      </div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link"> Home </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/subjects" class="nav-link"> Subjects </router-link>
+                    </li>
+                </ul>
+            </div>
 
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item mr-2">
-          <router-link to="/login" class="nav-link"> Sign in </router-link>
-        </li>
-
-      </ul>
-    </div>
-
-  </nav>
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item mr-2">
+                    <router-link v-if="!isAuthenticated" to="/login" class="nav-link"> Sign in </router-link>
+                    <span v-else class="nav-link"> Logged in </span>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <script>
 export default {
-  name: "AppHeader"
+    name: "AppHeader",
+    props: {
+        isAuthenticated: {
+            type: Boolean,
+            required: true
+        }
+    }
 }
 </script>
 
