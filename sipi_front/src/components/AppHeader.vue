26de896e347324cc59b4a4d362c6fe07d70e2af0
Maksim Kautarov <just5py@yandex.ru>
2023-04-27 17:43:32 +0300

    user profile popup window and favicon add

--- sipi_front/src/components/AppHeader.vue
+++ sipi_front/src/components/AppHeader.vue
@@ -4,35 +4,75 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                        <router-link to="/" class="nav-link"> Home </router-link>
+                        <router-link to="/" class="nav-link">Home</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link to="/subjects" class="nav-link"> Subjects </router-link>
+                        <router-link to="/subjects" class="nav-link">Subjects</router-link>
                     </li>
                 </ul>
             </div>
 
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item mr-2">
-                    <span class="nav-link"> Logged in </span>
+                    <span class="nav-link">Logged in</span>
+                </li>
+                <li class="nav-item">
+                    <button class="btn btn-primary" @click="openPopup">User Info</button>
                 </li>
             </ul>
         </div>
+
+        <UserInfoPopup v-if="isPopupOpen" :user-info="userInfo" @close="closePopup"/>
     </nav>
 </template>
 
 <script>
+import UserInfoPopup from './UserInfoPopup.vue';
+
+function getCookie(name) {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + "=")) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+
 export default {
     name: "AppHeader",
+    components: {
+        UserInfoPopup,
+    },
     props: {
         isAuthenticated: {
             type: Boolean,
             required: true
         }
-    }
-}
+    },
+    data() {
+        return {
+            isPopupOpen: false,
+            userInfo: null,
+        };
+    },
+
+
+    mounted() {
+        this.userInfo = JSON.parse(getCookie('user'));
+    },
+    methods: {
+        openPopup() {
+            this.isPopupOpen = true;
+        },
+        closePopup() {
+            this.isPopupOpen = false;
+        },
+    },
+};
 </script>
 
-<style scoped>
 
+<style scoped>
 </style>
---
+++ sipi_front/src/components/UserInfoPopup.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="user-info-popup">
+        <h2>Ваш профиль</h2>
+        <p>Username: {{ userInfo.username }}</p>
+        <p>Full Name: {{ userInfo.user_fullname }}</p>
+        <p>Personal Cipher: {{ userInfo.personal_cipher }}</p>
+        <p>Role: {{ userInfo.role }}</p>
+        <button class="exit-button" @click="$emit('close')">Exit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserInfoPopup",
+    props: {
+        userInfo: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+.user-info-popup {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 20px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-radius: 10px; /* Добавлено свойство border-radius */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+.exit-button {
+    background-color: #ef476f;
+    color: #fff;
+    font-size: 16px;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+</style>
